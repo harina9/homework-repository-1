523580ed7878fa8e80a15e39be0a1d275870ef1d
Arina Petrova <petrova.arina.s@yandex.ru>
2021-04-29 17:29:58 +0300

    Optimizing task4 with black

--- homework2/task4.py
+++ homework2/task4.py
@@ -17,12 +17,17 @@ def cache(func: Callable) -> Callable:
     stored_values = []
 
     def wrapper(*args, **kwargs):
-        key_of_call = args, kwargs
+        key_of_call = (args, kwargs)
         for key, value in stored_values:
             if key == key_of_call:
                 return value
-        result = func(*args, **kwargs)
-        stored_values.append((key_of_call, result))
-        return result
+        else:
+            result = func(*args, **kwargs)
+            stored_values.append((key_of_call, result))
+            return result
 
     return wrapper
+
+
+def func(a, b):
+    return (a ** b) ** 2
--- tests/homework2/test_task4.py
+++ tests/homework2/test_task4.py
@@ -1,15 +1,9 @@
-from homework2.task4 import cache
-
-counter = 0
+from homework2.task4 import cache, func
 
 
-def test_cache_decorator():
-    @cache
-    def cached_function():
-        global counter
-        counter += 1
-        assert counter == 0
-        cached_function()
-        assert counter == 1
-        cached_function()
-        assert counter == 1
+def test_cache_function():
+    cache_func = cache(func)
+    some = 100, 200
+    val_1 = cache_func(*some)
+    val_2 = cache_func(*some)
+    assert val_1 is val_2
