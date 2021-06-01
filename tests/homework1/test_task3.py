5bb6ce91878f080cb84dc44c8e6cc4f9c8439bb0
Arina Petrova <petrova.arina.s@yandex.ru>
2021-06-01 17:32:13 +0300

    Added path to test3

--- tests/homework1/test_task3.py
+++ tests/homework1/test_task3.py
@@ -2,12 +2,12 @@ from homework1.task3 import find_maximum_and_minimum
 
 
 def test_only_negative_numbers():
-    assert find_maximum_and_minimum("number1.txt") == (-56, -1)
+    assert find_maximum_and_minimum("tests/homework1/number1.txt") == (-56, -1)
 
 
 def test_only_positive_numbers():
-    assert find_maximum_and_minimum("number2.txt") == (2, 789)
+    assert find_maximum_and_minimum("tests/homework1/number2.txt") == (2, 789)
 
 
 def test_positive_and_negative():
-    assert find_maximum_and_minimum("number3.txt") == (-19, 78)
+    assert find_maximum_and_minimum("tests/homework1/number3.txt") == (-19, 78)
